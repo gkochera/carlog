5ff177647c4de2cfcd5a1b9615434e5bb57e7244
George Kochera <george.kochera@gmail.com>
2021-09-01 14:04:35 -0400

    reset the app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { SingleCarComponent } from './components/single-car/single-car.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    CarsComponent,
+    SingleCarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
