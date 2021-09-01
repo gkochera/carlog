e1862eb9aff3a76a13ede836d13554d978270159
George Kochera <george.kochera@gmail.com>
2021-09-01 12:22:49 -0400

    cleared out existing scratch work

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DisplayComponent } from './components/display/display.component';
-import { KeypadComponent } from './components/keypad/keypad.component';
-import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DisplayComponent,
-    KeypadComponent,
-    ButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
