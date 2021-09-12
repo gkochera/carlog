2a2b1d0d711023b0197253c075462a0a76266a2c
George Kochera <george.kochera@gmail.com>
2021-09-11 22:02:52 -0400

    styled the button to be more modern

--- client/src/app/components/button/button.component.ts
+++ client/src/app/components/button/button.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text!: string;
-  @Input() color!: string;
+  @Input() theme!: string;
   @Output() btnClick = new EventEmitter
   
 
