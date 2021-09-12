4221437e6f7424257648bce886619f37fe76829e
George Kochera <george.kochera@gmail.com>
2021-09-12 17:39:39 -0400

    wired up the signup and login form on the angular side, fixed the password fields

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // New User Form
+  signUpForm = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rptPassword: new FormControl('')
+  })
+
+  // Login Form
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  // UI States
+  showLoginForm: boolean = false;
+  showSignUpForm: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -14,10 +34,22 @@ export class HomeComponent implements OnInit {
 
   onSignUp(): void {
     console.log("Sign Up!")
+    console.log(this.signUpForm.value)
   }
 
   onLogin(): void {
     console.log("Login!")
+    console.log(this.loginForm.value)
+  }
+
+  toggleLoginForm() {
+    this.showLoginForm = !this.showLoginForm;
+    this.showSignUpForm = false;
+  }
+
+  toggleSignUpForm() {
+    this.showSignUpForm = !this.showSignUpForm;
+    this.showLoginForm = false;
   }
 
 }
